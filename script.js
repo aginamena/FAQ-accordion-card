db071f647f0b492be5afcdbe81e659ab7925643f
mena <ag5>
2021-02-22 10:44:48 -0500

    faq project

---
+++ script.js
@@ -0,0 +1,55 @@
+
+let question_one = document.querySelector("#question-one");
+question_one.addEventListener("click", ()=>{
+  question_one.style = "font-weight:700";
+  let answer_one = document.querySelector(".answer-one");
+  if (answer_one.style.display === "none") {
+    answer_one.style.display = "block";
+  } else {
+    answer_one.style.display = "none";
+  }
+});
+
+let question_two = document.querySelector("#question-two");
+question_two.addEventListener("click", ()=>{
+  question_two.style = "font-weight:700";
+  let answer_two = document.querySelector(".answer-two");
+  if (answer_two.style.display === "none") {
+    answer_two.style.display = "block";
+  } else {
+    answer_two.style.display = "none";
+  }
+});
+
+let question_three = document.querySelector("#question-three");
+question_three.addEventListener("click", ()=>{
+  question_three.style = "font-weight:700";
+  let answer_three = document.querySelector(".answer-three");
+  if (answer_three.style.display === "none") {
+    answer_three.style.display = "block";
+  } else {
+    answer_three.style.display = "none";
+  }
+});
+
+let question_four = document.querySelector("#question-four");
+question_four.addEventListener("click", ()=>{
+  question_four.style = "font-weight:700";
+  let answer_four = document.querySelector(".answer-four");
+  if (answer_four.style.display === "none") {
+    answer_four.style.display = "block";
+  } else {
+    answer_four.style.display = "none";
+  }
+});
+
+let question_five = document.querySelector("#question-five");
+question_five.addEventListener("click", ()=>{
+  question_five.style = "font-weight : 700";
+  let answer_five = document.querySelector(".answer-five");
+  if (answer_five.style.display === "none") {
+    answer_five.style.display = "block";
+  } else {
+    answer_five.style.display = "none";
+  }
+});
